8d80caaf0db67cab51e6d7ef2cc4cedaf71f0f2d
AugustoPB <tinho.pb@hotmail.com>
2016-07-15 19:42:54 -0300

    Modified client for Donnie Robot

--- client/DonnieClient.cpp
+++ client/DonnieClient.cpp
@@ -7,20 +7,20 @@ using namespace PlayerCc;
 
 DonnieClient::DonnieClient()
 {
-  robot = new PlayerClient("localhost",6665);
-  head = new PlayerClient("localhost",6666);
+  robot = new PlayerClient("192.168.0.103",6665);
+  //head = new PlayerClient("localhost",6666);
 
   p2dProxy = new Position2dProxy(robot,0);
-  p2d_headProxy = new Position2dProxy(head,1);
+  //p2d_headProxy = new Position2dProxy(head,1);
 
   bpProxy = new BumperProxy(robot,0);
 
-  BfinderProxy = new BlobfinderProxy(head,0);
+  //BfinderProxy = new BlobfinderProxy(head,0);
 
   sonarProxy = new RangerProxy(robot,0);
-  SHProxy = new RangerProxy(head,3);
+  //SHProxy = new RangerProxy(head,3);
 
-  speech = new SpeechProxy(robot,0);
+  //speech = new SpeechProxy(robot,0);
 
 }
 
@@ -109,7 +109,8 @@ void DonnieClient::ParaFrente(float arg)
 	for(int i = 1; i <= (int)arg; i++)
 	{
 
-	  path.push_back((float)i);
+	  path.push_back(i);
+	  cout << path[i-1] << endl;
 	}
 
 	float fltPart = arg - (int)arg;
@@ -128,10 +129,10 @@ void DonnieClient::ParaFrente(float arg)
 	float andou;
 
 	robot->Read();
-	if(sonarProxy->GetRange(0) > 1.6)
+	if(sonarProxy->GetRange(1) > 1.6) // For donnie, do not work in stage.
 	{
 	  stop = false;
-	  p2d_headProxy->SetSpeed(1,0);
+	  //p2d_headProxy->SetSpeed(1,0);
 	  p2dProxy->SetSpeed(1,0);
 	}
 
@@ -139,18 +140,18 @@ void DonnieClient::ParaFrente(float arg)
 	while(!stop)
 	{
 
-		cout << passos << " > " << Npassos << endl;
+		//cout << passos << " > " << Npassos << endl;
 	  if(passos > Npassos )
 	  {
-	  	p2d_headProxy->SetSpeed(0,0);
+	  	//p2d_headProxy->SetSpeed(0,0);
 	    p2dProxy->SetSpeed(0,0);
 	    break;
 	  }
 
 	  robot->ReadIfWaiting();
-	  if(this->FrontBumper() != 0 or sonarProxy->GetRange(0) < 0.4 or sonarProxy->GetRange(2) < 0.65 or sonarProxy->GetRange(3) < 0.65)
+	  if(this->FrontBumper() != 0 or sonarProxy->GetRange(1) < 0.4 or sonarProxy->GetRange(2) < 0.65 or sonarProxy->GetRange(0) < 0.65) // For donnie, do not work in stage.
 	  {
-	  	p2d_headProxy->SetSpeed(0,0);
+	  	//p2d_headProxy->SetSpeed(0,0);
 	    p2dProxy->SetSpeed(0,0);
 	    stop = true;
 	    andou = sqrt(pow((p2dProxy->GetXPos() - posxi),2) + pow((p2dProxy->GetYPos() - posyi),2));
@@ -159,7 +160,7 @@ void DonnieClient::ParaFrente(float arg)
 	  }
 	    
 	  robot->ReadIfWaiting();
-	  if(sonarProxy->GetRange(0) < 1.5 and !obstacle)
+	  if(sonarProxy->GetRange(1) < 1.5 and !obstacle) // For donnie, do not work in stage.
 	  {
 	    Npassos = passos;
 	    obstacle = true;
@@ -236,8 +237,8 @@ void DonnieClient::ParaTras(float arg)
 	for(int i = 1; i <= (int)arg; i++)
 	{
 
-	  path.push_back((float)i);
-	  cout << path[i] << endl;
+	  path.push_back(i);
+	  cout << path[i-1] << endl;
 	}
 
 	float fltPart = arg - (int)arg;
@@ -256,10 +257,10 @@ void DonnieClient::ParaTras(float arg)
 	float erro;
 	float andou;
 
-	if(sonarProxy->GetRange(1) > 1.6)
+	if(sonarProxy->GetRange(3) > 1.6) // For donnie, do not work in stage.
 	{
 	  stop = false;
-	  p2d_headProxy->SetSpeed(-1,0);
+	  //p2d_headProxy->SetSpeed(-1,0);
 	  p2dProxy->SetSpeed(-1,0);
 	}
 
@@ -267,15 +268,15 @@ void DonnieClient::ParaTras(float arg)
 	{
 	  if(passos > Npassos )
 	  {
-	  	p2d_headProxy->SetSpeed(0,0);
+	  	//p2d_headProxy->SetSpeed(0,0);
 	    p2dProxy->SetSpeed(0,0);
 	    break;
 	  }
 
 	  robot->ReadIfWaiting();
-	  if(this->BackBumper() != 0 or sonarProxy->GetRange(1) < 0.4 or sonarProxy->GetRange(4) < 0.65 or sonarProxy->GetRange(5) < 0.65)
+	  if(this->BackBumper() != 0 or sonarProxy->GetRange(4) < 0.4 or sonarProxy->GetRange(3) < 0.65 or sonarProxy->GetRange(5) < 0.65) // For donnie, do not work in stage.
 	  {
-	  	p2d_headProxy->SetSpeed(0,0);
+	  	//p2d_headProxy->SetSpeed(0,0);
 	    p2dProxy->SetSpeed(0,0);
 	    stop = true;
 	    andou = sqrt(pow((p2dProxy->GetXPos() - posxi),2) + pow((p2dProxy->GetYPos() - posyi),2));
@@ -284,7 +285,7 @@ void DonnieClient::ParaTras(float arg)
 	  }
 	    
 	  robot->ReadIfWaiting();
-	  if(sonarProxy->GetRange(1) < 1.5 and !obstacle)
+	  if(sonarProxy->GetRange(4) < 1.5 and !obstacle) // For donnie, do not work in stage.
 	  {
 	    Npassos = passos;
 	    obstacle = true;
@@ -368,7 +369,7 @@ void DonnieClient::ParaDireita(float arg)
 
 	cout << yawd << endl;
 
-	p2d_headProxy->SetSpeed(0,-0.5);
+	//p2d_headProxy->SetSpeed(0,-0.5);
 	p2dProxy->SetSpeed(0,-0.5);
 	while(true)
 	{
@@ -518,7 +519,7 @@ void DonnieClient::ParaDireita(float arg)
 	  }
 	}
 
-	p2d_headProxy->SetSpeed(0,0);
+	//p2d_headProxy->SetSpeed(0,0);
 	p2dProxy->SetSpeed(0,0);
 	robot->Read();
 	cout << p2dProxy->GetYaw() << endl;
@@ -541,7 +542,7 @@ void DonnieClient::ParaEsquerda(float arg)
 	else
 	  yawd = yawi + degTOrad(arg);
 
-	p2d_headProxy->SetSpeed(0,0.5);
+	//p2d_headProxy->SetSpeed(0,0.5);
 	p2dProxy->SetSpeed(0,0.5);
 
 	while(true)
@@ -688,7 +689,7 @@ void DonnieClient::ParaEsquerda(float arg)
 	    break;
 	  }
 	}
-	p2d_headProxy->SetSpeed(0,0);
+	//p2d_headProxy->SetSpeed(0,0);
 	p2dProxy->SetSpeed(0,0);
 }
 
