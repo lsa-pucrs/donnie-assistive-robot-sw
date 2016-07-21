d89ec9e9bf45050e50dca56e34542761469211d1
Guilherme Marques <guilherme.marques.002@acad.pucrs.br>
2016-07-21 19:03:56 -0300

    test odometria angular

---
+++ driver/gtts/build/CMakeFiles/googletts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/googletts.dir/googletts.o"
+  "libgoogletts.pdb"
+  "libgoogletts.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/googletts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
