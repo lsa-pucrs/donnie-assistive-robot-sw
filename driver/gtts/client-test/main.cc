5d8b2deba263f522eecbe0efffdeac6bc3f905eb
Guilherme Marques <guilherme.marques.002@acad.pucrs.br>
2016-07-21 19:03:56 -0300

    test odometria angular

---
+++ driver/gtts/client-test/main.cc
@@ -0,0 +1,61 @@
+/*
+Copyright (c) 2005, Brad Kratochvil, Toby Collett, Brian Gerkey, Andrew Howard, ...
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of the Player Project nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <libplayerc++/playerc++.h>
+#include "args.h"
+#include <iostream>
+#if !defined (WIN32)
+  #include <unistd.h>
+#endif
+int main(int argc, char** argv)
+{
+  parse_args(argc, argv);
+
+  try
+  {
+    PlayerCc::PlayerClient client(gHostname, gPort);
+    PlayerCc::SpeechProxy sp(&client, gIndex);
+
+    sp.Say("O mundo inteiro é um palco\n");
+    usleep(3000000);
+    sp.Say("E todos os homens e mulheres são meros atores\n");
+    usleep(3000000);
+    sp.Say("Têm suas saídas e suas entradas\n");
+    usleep(3000000);
+    sp.Say("E um homem cumpre em seu tempo muitos papéis\n");
+    usleep(3000000);
+    sp.Say("Trecho da peça As You Like It, escrito por William Shakespeare\n");
+    usleep(3000000);
+  }
+  catch (PlayerCc::PlayerError & e)
+  {
+    std::cerr << e << std::endl;
+    return -1;
+  }
+  return 1;
+}
