7aa9c038a00b66366e9c5a201cc820400faeea1a
Guilherme Marques <guilherme.marques.002@acad.pucrs.br>
2016-09-02 15:39:39 -0300

    alerts compilando com esqueleto para fazer as partes de som

---
+++ driver/alerts/build/CMakeFiles/alerts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/alerts.dir/alerts.o"
+  "libalerts.pdb"
+  "libalerts.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/alerts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
