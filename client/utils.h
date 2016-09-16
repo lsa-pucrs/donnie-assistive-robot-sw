eb8bf7b8717c280f747d8640babc204457e5667b
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2016-09-16 11:03:22 -0300

    ajustes e merge dos branchs

---
+++ client/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <libplayerc++/playerc++.h>
+
+double radTOrad(double rad);
+
+double radTOdeg(double rad);
+
+double degTOrad(double deg);
+
+
+#endif
