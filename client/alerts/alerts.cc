f2820abde56db5daf70e07a37ac00918854a08db
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2016-09-21 21:39:02 -0300

    adicionado diretorio firmware e feita versao validacao do client alerts

--- client/alerts/alerts.cc
+++ client/alerts/alerts.cc
@@ -1,14 +1,169 @@
-#include <iostream>
-#include <libplayerc++/playerc++.h>
-#define RSCPATH "../resources/sounds/"
+#include "alerts.h"
+#define RSCPATH "../resources/sounds/" //relativo a pasta d server_donnie.cfg
+#define SSTEP RSCPATH"HIT.wav" //Sound STEP
+#define SSBACK RSCPATH"beep-01a.wav" //Sound Step BACK
+#define STRIGHT RSCPATH"right.wav" //Sound Turn RIGHT
+#define STLEFT RSCPATH"left.wav" //Sound Turn LEFT
+#define SBUMPER RSCPATH"mgs4_soundalert.wav" //Sound Turn LEFT
+#define SRANGER RSCPATH"mgs4_soundalert.wav" //Sound Turn LEFT
+
+
+#define STEP_YAW 10 //gradianos
+#define STEP_LENGHT 0.025  //TODO ver se esta certo com o client do augusto
+#define SIDE_RANGER 0.05   //TODO ver se esta certo com o client do augusto
+#define FRONT_RANGER 0.06   //TODO ver se esta certo com o client do augusto
+#define BACK_RANGER 0.05  //TODO ver se esta certo com o client do augusto
+
+
+DonnieClient::DonnieClient()
+{
+    string host = GetEnv("DONNIE_HOST");
+    int port = atoi(GetEnv("DONNIE_PORT").c_str());
+    if(host.size()==0) host = "localhost";
+    if(port==0) port = 6665;
+
+    robot = new PlayerClient(host,port);
+    //head = new PlayerClient("localhost",6666);
+    p2d = new Position2dProxy(robot,0);
+    //p2d_headProxy = new Position2dProxy(robot,1);
+    //actuator = new ActArrayProxy(robot,0);
+    bumper = new BumperProxy(robot,0);
+    //BfinderProxy = new BlobfinderProxy(head,0);
+    ranger = new RangerProxy(robot,0);
+    //SHProxy = new RangerProxy(head,3);
+    sound = new SoundProxy(robot, 0);
+
+    speech = new SpeechProxy(robot,0);
+
+
+    setPos(p2d->GetXPos(),p2d->GetYPos(),p2d->GetYaw());
+    double translation = 0;
+    double rotation = 0;
+    steps = 0;
+    alertStepFlag = 0;
+    alertBumperFlag = 0;
+    alertRangerFlag = 0;
+    robot->StartThread();
+}
+
+pos_t DonnieClient::getPos(){
+    return pos;
+}
+
+void DonnieClient::setPos(double x, double y, double a){
+    pos.x=x;
+    pos.y=y;
+    pos.a=a;
+}
+
+void DonnieClient::checkDir(){
+    robot->ReadIfWaiting();
+
+    /*system("clear");
+    cout << "==================" << endl;
+    cout << "POS(" << p2d->GetXPos() << ", " 
+                    << p2d->GetYPos() << ", "
+                    << radTOdeg(p2d->GetYaw()) << ")" << endl;*/
+
+    translation = hypotf(p2d->GetXPos() - pos.x, p2d->GetYPos() - pos.y) - translation;
+    //cout << "translation:" << translation << endl;
+    //cout << "steps:" << steps << endl;
+    
+    if(translation>=STEP_LENGHT){  //if(translation>(steps+1)*STEP_LENGHT){
+        if(p2d->GetXPos()>pos.x){
+            steps++;
+            sound->play(SSTEP);
+            cout << "forward:" << translation << endl;
+            cout << "steps:" << steps << endl << endl;
+        }
+        else{
+            steps--;
+            sound->play(SSBACK);
+            cout << "backward:" << translation << endl;
+            cout << "steps:" << steps << endl << endl;
+        }
+        setPos(p2d->GetXPos(),p2d->GetYPos(),pos.y);
+        translation=0; 
+        //cout << "gtranslation:" << translation << endl;
+        //cout << "gsteps:" << steps << endl;
+    }
+
+
+    //cout << "Yaw:" << radTOdeg(p2d->GetYaw()) << endl << endl;
+    if((p2d->GetYaw()>pos.a)){ //ge
+        if(radTOdeg(p2d->GetYaw() - pos.a)>STEP_YAW){
+            cout << "ge Yaw-a:" << radTOdeg(p2d->GetYaw() - pos.a) << endl << endl;
+            setPos(pos.x,pos.y,p2d->GetYaw());
+            sound->play(STLEFT);
+        }
+    }
+    else{ //gd
+        if(radTOdeg(pos.a - p2d->GetYaw())>STEP_YAW){
+            cout << "gd Yaw-a:" << radTOdeg(pos.a - p2d->GetYaw()) << endl << endl;
+            setPos(pos.x,pos.y,p2d->GetYaw());
+            sound->play(STRIGHT);
+        }
+    }
+    
+
+}
+
+void DonnieClient::checkBumpers(){
+    robot->ReadIfWaiting();
+
+    if(bumper->IsAnyBumped()){
+        if(!alertBumperFlag){
+            cout << "BUMPED!:" << endl << endl;
+            alertBumperFlag=1;
+            sound->play(SBUMPER);
+            speech->Say(" Alguma coisa se colidiu com o robô\n");
+        }
+    }
+    else alertBumperFlag = 0;
+}
+
+void DonnieClient::checkRangers(){
+    robot->Read();
+    ///100 to convert from metters to cm
+    if ((ranger->GetRange(0))<SIDE_RANGER || //fr 
+        (ranger->GetRange(1))<FRONT_RANGER || //front
+        (ranger->GetRange(2))<SIDE_RANGER || //fl
+        (ranger->GetRange(3))<SIDE_RANGER || //bl
+        (ranger->GetRange(4))<BACK_RANGER || //back
+        (ranger->GetRange(5))<SIDE_RANGER){  //br
+       
+        if(!alertRangerFlag){
+            cout << "RANGED!:" << endl << endl;
+            for(int i=0;i<6;i++){
+               cout <<"ranger"<< i << ":" << (ranger->GetRange(i)) << endl;
+            }
+            cout << endl;
+            alertRangerFlag = 1;
+            sound->play(SRANGER);
+            //speech->Say(" Existem obstaculos no caminho\n");
+        }
+    }
+    else alertRangerFlag = 0;
+}
+
+string GetEnv( const string & var ) 
+{
+     const char * val = ::getenv( var.c_str() );
+     if ( val == 0 ) {
+         return "";
+     }
+     else {
+         return val;
+     }
+}
 
 int main(int argc, char *argv[]){
-	using namespace PlayerCc;
-	//PlayerClient    robot("192.168.0.113",6665);
-	PlayerClient    robot("localhost",6665);
-	SoundProxy     sound(&robot, 0);
-	if(argc>1)sound.play(argv[1]);
-	else sound.play(RSCPATH"right.wav");
+    DonnieClient *donnie1 = new DonnieClient();
+    while(1){
+        donnie1->checkDir();
+        donnie1->checkBumpers();
+        donnie1->checkRangers();
+    }
 
-	return 0;
+    return 0;
 }
---
+++ firmware/config.h
@@ -0,0 +1,114 @@
+/*aqui vao os a declaracao dos pinos e as definicoes estaticas do donnie tais como carga
+maxima da bateria e carga minima e quantidade de sensores sendo usados etc
+
+
+*/
+
+/*
+USAR IDE ARDUINO 1.2 OU SUPERIOR 
+
+ http://forum.arduino.cc/index.php?topic=45742.0 - checksum
+ LiquidCrystal Library - Serial Input
+ The lcd circuit:
+ VDD VCC
+ VSS GND
+
+ LCD R/W pin to ground
+ 10K potenciometer to LCD VO 
+*/
+
+
+#ifndef config_h
+#define config_h
+
+#include <Arduino.h>
+
+
+//BATERRY PIN
+#define PIN_BATTERY 0 //Analogic input
+
+//SYSTEM DIO PINS
+#define PIN_LED_BATTERY 52
+#define PIN_LED_CONNECTION 50
+#define PIN_VIBRATION_R 48
+#define PIN_VIBRATION_L 38
+#define PIN_BUZZER 36
+
+//BUMPER PINS
+#define PIN_BUMPER_NE 47
+#define PIN_BUMPER_NW 49
+#define PIN_BUMPER_SW 51
+#define PIN_BUMPER_SE 53
+
+//DANIEL MOTORS DRIVER PINS
+//Motor A (RIGHT)
+#define RPWMA 13
+#define RAIN1 8
+#define RAIN2 12
+ //Motor B (LEFT)
+#define LPWMB 11
+#define LBIN1 9
+#define LBIN2 10
+ //Motors standby mode
+#define STBY 7 //This pi9n must be driven high to take the driver out of standby mode.
+ //encoder
+#define pololuAL 0 //pin 2
+#define pololuBL 34
+#define pololuAR 1 //pin 3
+#define pololuBR 33
+//#define kP 3
+//#define kI 0 // nao utilizado
+//#define kD 0 // nao utilizado
+
+/*
+
+//MOTORS DRIVER PINS
+//Motor A (LEFT)
+#define PWMA 11 //Speed control 
+#define AIN1 9 //Direction
+#define AIN2 10 //Direction
+//Motor B (RIGHT)
+#define PWMB 13 //Speed control
+#define BIN1 8 //Direction
+#define BIN2 12 //Direction
+//Motors standby mode
+#define STBY 7 //This pi9n must be driven high to take the driver out of standby mode.
+*/
+
+ //SERVO PIN
+#define PIN_SERVO_NECK 25
+
+//ENCODER PINS
+#define INTERRUPT_ENCODER_R_M1A 1 //1 = int1 pin3
+#define INTERRUPT_ENCODER_L_M2A 0 //0 = int0 pin2
+#define PIN_ENCODER_R_M1B 33
+#define PIN_ENCODER_L_M2B 34
+
+//BATERRY PARAMETERS
+#define BATTERY_FULL 8.3  //for S2 4.15v each cell
+#define BATTERY_LOW_BATTERY_ALERT 7 //7
+#define BATTERY_EMPTY 6.6 //for S2 3.3v each cell
+#define ANALOG_EQUIV_FULL 535 //the value receiveid in the arduino pin equivalent of full voltage (ex: 535 = 8.4v)
+#define ANALOG_EQUIV_EMPTY 430
+
+//UPDATE FREQUENCY PARAMETERS
+#define UPDATE_DIO_FRQ 50  
+#define UPDATE_RANGER_FRQ 150
+#define UPDATE_BUMPER_FRQ 50
+#define UPDATE_BATTERY_FRQ 50
+#define UPDATE_ENCODER_FRQ 50
+#define UPDATE_MOTORS_FRQ 50
+#define UPDATE_SERVOS_FRQ 50
+//#define UPDATE_PING_FRQ 50
+
+//SEND FREQUENCY PARAMETERS
+#define SEND_DIO_FRQ 300
+#define SEND_RANGER_FRQ 300
+#define SEND_BUMPER_FRQ 300
+#define SEND_BATTERY_FRQ 300
+#define SEND_ENCODER_FRQ 300
+#define SEND_SYSTEMMSG_FRQ 300
+#define SEND_REQUESTCONFIG_FRQ 1000
+#define SEND_PING_FRQ 2000
+
+#endif
