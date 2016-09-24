021540ef4068a467c745cbe26a0d4bbd706b27d3
Alexandre Amory <amamory@gmail.com>
2016-09-24 14:52:11 -0300

    opencv cmake

--- driver/genericblobfinder/GenericBlobfinder.cc
+++ driver/genericblobfinder/GenericBlobfinder.cc
@@ -46,14 +46,14 @@ driver
 #include <iostream>
 #include <libplayercore/playercore.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/videoio/videoio.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/video/video.hpp>
+//#include <opencv2/videoio/videoio.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/calib3d/calib3d.hpp>
+//#include <opencv2/features2d/features2d.hpp>
 
 using namespace cv;
 using namespace std;
