653f2431a5f0066ee462ed9d2b963415f21a4de3
Alexandre Amory <amamory@gmail.com>
2016-10-13 12:45:49 +0000

    fix raspdriver client example

--- driver/raspicamdriver/client/main.cc
+++ driver/raspicamdriver/client/main.cc
@@ -3,13 +3,13 @@
 #include <libplayerc++/playerc++.h>
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/videoio/videoio.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/video/video.hpp>
+//#include <opencv2/videoio/videoio.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/calib3d/calib3d.hpp>
+//#include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
 using namespace cv;
