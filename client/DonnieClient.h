8c3cd0a61e0614f2bfa585489be931a05e76aa05
Henry Nunes <henry.nunes@outlook.com>
2016-10-14 11:04:02 -0300

    Removido Historic de dentro do DonnieCLient

--- client/DonnieClient.h
+++ client/DonnieClient.h
@@ -21,44 +21,6 @@ struct PathNodes
   double posx, posy;
 };
 
-//Class to Keep the Commands History
-class Historic
-{	
-	private:
-		//Struct to save commands, no external access alowed
-		struct Command {
-			string name;
-			string feedback;
-			double posx;
-			double posy;
-			double degree;
-		};
-		list<Command> commandsList;
-		//Bidiretional Iterator
-		list<Command>::iterator iterator;
-		bool resetIterator();
-		
-		//Variable to hold Singleton 
-		static Historic singleton;
-		
-		//Block the construction and destruction of the object
-		Historic();
-		~Historic();
-			
-	public:
-		//Method to get the Singleton
-		static Historic& getHistoric();
-		
-		
-		bool addCommand(string name, string feedback);
-		int size();
-		string getLast();
-		string getLine(int line);
-		string previous();
-		string next();
-};
-
-
 //Class to comunicate with Player Proxy
 //Work in progress to turn in a Singleton
 //The default Player library proxies can be found at http://playerstage.sourceforge.net/doc/Player-2.0.0/player/classPlayerCc_1_1ClientProxy.html
