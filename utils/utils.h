9ef50ce87c592548cae781de6ed26eb2377267d7
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2016-11-09 11:35:29 -0200

    adicionado GetEnv e macro DEBUG_MSG como mensagens para o desenvolvedor

--- utils/utils.h
+++ utils/utils.h
@@ -2,6 +2,22 @@
 #define UTILS_H_
 
 #include <libplayerc++/playerc++.h>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+// Message macros
+#ifdef NDEBUG
+//! Debug messages. They are not showed when NDEBUG are not defined
+/*! Especify the parameters by << separator \n
+	Example: DEBUG_MSG("test" << var << "Kmh")*/
+#define DEBUG_MSG(msg) cout << msg << endl << flush
+#else  //avoid errors if NDEBUG are not defined and someone use the defines with parameters
+#define DEBUG_MSG(msg) //void
+#endif
+
 
 double radTOrad(double rad);
 
@@ -9,5 +25,16 @@ double radTOdeg(double rad);
 
 double degTOrad(double deg);
 
+//! Allow to use environments variables
+/*!	Used to avoid the need to recompile all code for little changes.\n
+	Examples: \n
+	To test if the variable exists: \n
+	if(GetEnv("IP").size()==0) \n
+	or \n
+	if(GetEnv("IP")=="") \n
+	To set variable you can use "$export IP=localhost"
+	\param string var with the required environment
+	\return string with the value in the environment parameter. Returns "" with non initialized param.*/
+string GetEnv( const string & var );	
 
 #endif
