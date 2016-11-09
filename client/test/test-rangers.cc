51a596183bc73d591386e2f462559cdd5589cc85
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2016-11-09 11:36:57 -0200

    adicionado cliente para teste dos rangers

---
+++ client/test/test-rangers.cc
@@ -0,0 +1,36 @@
+#define NDEBUG
+#include <iostream>
+#include <libplayerc++/playerc++.h>
+#include "utils.h"
+
+
+int main(int argc, char *argv[]){
+	using namespace PlayerCc;
+	using namespace std;
+
+	string host = GetEnv("DONNIE_IP");
+	int port = atoi(GetEnv("DONNIE_PORT").c_str());
+	if(host.size()==0) host = "localhost";
+	if(port==0) port = 6665;
+
+	//GetEnv("DONNIE_IP").size()==0 ? "localhost" : GetEnv("DONNIE_IP")
+	//atoi(GetEnv("DONNIE_PORT").c_str())==0 ? 6665 : atoi(GetEnv("DONNIE_PORT").c_str()) 
+
+	PlayerClient robot(host,port);
+	RangerProxy	bodySonarProxy(&robot, 0);
+	RangerProxy	headSonarProxy(&robot, 1);
+	int i=0;
+
+	while(true){
+		robot.Read();
+		cout << "Qnt body:" << bodySonarProxy.GetRangeCount() << endl;
+		for(i=0;i<bodySonarProxy.GetRangeCount();i++) cout << bodySonarProxy[i] << " " << endl;
+		cout << endl;
+		cout << "Qnt head:" << headSonarProxy.GetRangeCount() << endl;
+		for(i=0;i<headSonarProxy.GetRangeCount();i++) cout << headSonarProxy[i] << endl;
+		cout << endl << endl;
+		sleep(2);
+	}
+
+	return 0;
+}
