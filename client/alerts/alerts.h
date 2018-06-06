3b6495156ff8e25d18e3461ab052dfe54481feaf
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-06-06 18:19:27 -0300

    All client/alerts using clang-format with GOOGLE style

--- client/alerts/alerts.h
+++ client/alerts/alerts.h
@@ -1,55 +1,53 @@
 //marques lsa
+#include <libplayerc++/playerc++.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#include <iostream>
 #include <cmath>
+#include <iostream>
+#include <string>
 #include "utils.h"
-#include <libplayerc++/playerc++.h>
 
 using namespace std;
 using namespace PlayerCc;
 
-typedef struct PathNodes{
-  double x, y, a;
+typedef struct PathNodes {
+    double x, y, a;
 } pos_t;
 
+class DonnieClient {
+    PlayerClient *robot;
+    //PlayerClient *head;
+    Position2dProxy *p2d;
+    Position2dProxy *p2dhead;
+    //ActArrayProxy *actuator;
+    BumperProxy *bumper;
+    //BlobfinderProxy *BfinderProxy;
+    RangerProxy *ranger;
+    //RangerProxy *SHProxy;
+    //SpeechProxy *speech;
+    SoundProxy *sound;
+    pos_t pos;
+    double translation;
+    double translationError;
+    double rotation;
+    double rotationError;
+    int alertBumperFlag;
+    int alertRangerFlag;
 
-class DonnieClient{
-  PlayerClient *robot;
-  //PlayerClient *head;
-  Position2dProxy *p2d;
-  Position2dProxy *p2dhead;
-  //ActArrayProxy *actuator;
-  BumperProxy *bumper;
-  //BlobfinderProxy *BfinderProxy;
-  RangerProxy *ranger;
-  //RangerProxy *SHProxy;
-  //SpeechProxy *speech;
-  SoundProxy *sound;
-  pos_t pos;
-  double translation;
-  double translationError;
-  double rotation;
-  double rotationError;
-  int alertBumperFlag;
-  int alertRangerFlag;
+    string SSTEP;    //Sound STEP
+    string SSBACK;   //Sound Step BACK
+    string STRIGHT;  //Sound Turn RIGHT
+    string STLEFT;   //Sound Turn LEFT
+    string SBUMPER;  //Sound Turn LEFT
+    string SRANGER;  //Sound Turn LEFT
+    string SSCAN;    //Sound Scan
 
-  string SSTEP;   //Sound STEP
-  string SSBACK;  //Sound Step BACK
-  string STRIGHT; //Sound Turn RIGHT
-  string STLEFT;  //Sound Turn LEFT
-  string SBUMPER; //Sound Turn LEFT
-  string SRANGER; //Sound Turn LEFT
-  string SSCAN; //Sound Scan
-
-
-public:
-  DonnieClient();
-  pos_t getPos();
-  void setPos(double x, double y, double a);
-  void checkSteps();
-  void checkHead();
-  void checkBumpers();
-  void checkRangers();
+   public:
+    DonnieClient();
+    pos_t getPos();
+    void setPos(double x, double y, double a);
+    void checkSteps();
+    void checkHead();
+    void checkBumpers();
+    void checkRangers();
 };
