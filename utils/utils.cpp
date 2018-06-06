de6274b4621898c4dbf141d891b2839ce8088ded
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-06-06 18:14:48 -0300

    All utils using clang-format with GOOGLE style

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -2,63 +2,53 @@
 
 using namespace PlayerCc;
 
-
 //////////////Converte radianos negativos ou positivos em radianos positivos EX. -1,57079 = 4,71238/////////////////////
-double radTOrad(double rad)
-{
-	if(rad < 0)
-	{
-		rad = 2*M_PI + rad;
-	}
-	return rad;
+double radTOrad(double rad) {
+    if (rad < 0) {
+	rad = 2 * M_PI + rad;
+    }
+    return rad;
 }
 
 ////////////////////Converte radianos negativos ou positivos em graus positivos EX. -1,57079 = 270///////////////////////
-double radTOdeg(double rad)
-{
-	if(rad < 0)
-		rad = 360 + rtod(rad);
-	else
-		rad = rtod(rad);
-	return rad;
+double radTOdeg(double rad) {
+    if (rad < 0)
+	rad = 360 + rtod(rad);
+    else
+	rad = rtod(rad);
+    return rad;
 }
 
 ///////////////////Converte graus negativos ou positivos em radianos positivos EX. -60 = 5,23598/////////////////////////
 
-double degTOrad(double deg)
-{
-	if(deg < 0)
-		deg = 2*M_PI + dtor(deg);
-	else
-		deg = dtor(deg);
-	return deg;
+double degTOrad(double deg) {
+    if (deg < 0)
+	deg = 2 * M_PI + dtor(deg);
+    else
+	deg = dtor(deg);
+    return deg;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-string toLowerCase(string input)
-{
-	for(int i = 0; input[i]; i++)
-	{
-		if(input[i] <= 90 && input[i] >= 65)
-			input[i] = tolower(input[i]);
-		else if(input[i] <= -60 && input[i] >= -90) //Converts ISO 8859-1 character with accent to lowercase
-			input[i] = input[i] + 32;
-	}
-
-	return input;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-string GetEnv( const string & var ) 
-{
-    const char * val = ::getenv( var.c_str() );
-    if ( val == 0 ) {
-       DEBUG_MSG("\033[41mWarning!\033[0m " << var.c_str() << " environment variable are not set!"); //TODO BUG -> Essa macro nesse arquivo nunca executa pois o NDEBUG nunca esta setado. Aparentemente o utils.h compila antes do main.cc nesse meu makefile
-       return "";
+string toLowerCase(string input) {
+    for (int i = 0; input[i]; i++) {
+	if (input[i] <= 90 && input[i] >= 65)
+	    input[i] = tolower(input[i]);
+	else if (input[i] <= -60 && input[i] >= -90)  //Converts ISO 8859-1 character with accent to lowercase
+	    input[i] = input[i] + 32;
     }
-    else {
-       return val;
+
+    return input;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+string GetEnv(const string& var) {
+    const char* val = ::getenv(var.c_str());
+    if (val == 0) {
+	DEBUG_MSG("\033[41mWarning!\033[0m " << var.c_str() << " environment variable are not set!");  //TODO BUG -> Essa macro nesse arquivo nunca executa pois o NDEBUG nunca esta setado. Aparentemente o utils.h compila antes do main.cc nesse meu makefile
+	return "";
+    } else {
+	return val;
     }
 }
--- utils/utils.h
+++ utils/utils.h
@@ -2,9 +2,9 @@
 #define UTILS_H_
 
 #include <libplayerc++/playerc++.h>
+#include <stdio.h>
 #include <iostream>
 #include <string>
-#include <stdio.h>
 
 using namespace std;
 
@@ -13,11 +13,11 @@ using namespace std;
 //! Debug messages. They are not showed when NDEBUG are not defined
 /*! Especify the parameters by << separator \n
 	Example: DEBUG_MSG("test" << var << "Kmh")*/
-#define DEBUG_MSG(msg) cout << msg << endl << flush
-#else  //avoid errors if NDEBUG are not defined and someone use the defines with parameters
-#define DEBUG_MSG(msg) //void
+#define DEBUG_MSG(msg) cout << msg << endl \
+			    << flush
+#else			//avoid errors if NDEBUG are not defined and someone use the defines with parameters
+#define DEBUG_MSG(msg)  //void
 #endif
-
 
 double radTOrad(double rad);
 
@@ -37,6 +37,6 @@ string toLowerCase(string input);
 	To set variable you can use "$export IP=localhost"
 	\param string var with the required environment
 	\return string with the value in the environment parameter. Returns "" with non initialized param.*/
-string GetEnv( const string & var );	
+string GetEnv(const string& var);
 
 #endif
