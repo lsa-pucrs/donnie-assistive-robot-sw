cdcf0c942e2475a487f552383692d44d13fbb074
Alexandre Amory <amamory@gmail.com>
2018-10-13 20:48:09 -0400

    renaming client to ranger2vib

--- client/ranger2vib/ranger2vib.cc
+++ client/ranger2vib/ranger2vib.cc
@@ -1,26 +1,26 @@
-#include <vib_belt.h>
+#include <ranger2vib.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    vib_belt* belt = NULL;
+    ranger2vib* belt = NULL;
 
     // treatment for the arguments
     if (argc > 1){
         if (argv[1][0] == '-'){
             switch (argv[1][1]) {
                 case 'p':
-                    belt = new vib_belt(argv[2], atoi(argv[3]));
+                    belt = new ranger2vib(argv[2], atoi(argv[3]));
                     break;
                 default:
                     cout << "The list of know parameters are: " << endl;
-                    cout << "To run the belt on a user defined serial port, host and port: ./vib_belt -p <hostname> <port>" << endl;
+                    cout << "To run the belt on a user defined serial port, host and port: ./ranger2vib -p <hostname> <port>" << endl;
                     return 0;
             }
 		}
     }else{
-        // default vib_belt constructor
-        belt = new vib_belt();
+        // default ranger2vib constructor
+        belt = new ranger2vib();
 	}
 	
 	if (belt == NULL){
