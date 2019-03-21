26a547d9cd99a5762af9a1f11a5ac6860cd0b31f
IgorSouzaA <igor.azevedo@acad.pucrs.br>
2019-03-21 15:36:38 -0300

    Updated new Arduino software behaviour for errors

--- firmware/vib_belt/vib_belt_new/vib_belt_new.ino
+++ firmware/vib_belt/vib_belt_new/vib_belt_new.ino
@@ -6,18 +6,18 @@
 
 struct MotorPin_Out {
 
-  char motPin;
+  byte motPin;
   
-  char motOutputLevel;
+  byte motOutputLevel;
 
 };
 
-//Arduino MEGA
+//Arduino MEGA - 6 PWM Pins
 MotorPin_Out arduinoPins[PINOUT_LENGTH] = {
   { 8 , 0 }, { 9 , 0 }, { 10 , 0 }, { 11 , 0 }, { 12 , 0 }, { 13 , 0 }
 };
 
-//Arduino NANO
+//Arduino NANO - 6 PWM Pins
 /*MotorPin_Out arduinoPins[PINOUT_LENGTH] = {
   { 3 , 0 }, { 5 , 0 }, { 6 , 0 }, { 9 , 0 }, { 10 , 0 }, { 11 , 0 }
 };*/
@@ -35,7 +35,7 @@ void setup() {
   }
 }
 
-int countTimeOut = 0;
+unsigned int countTimeOut = 0;
 
 int  m_nFrameDataPos = -1;
 
@@ -45,7 +45,7 @@ void loop() {
   while ( !Serial.available() ) {
     
     //Decrementa o tempo de TimeOut caso seja diferente de zero
-    if ( countTimeOut ){
+    if ( countTimeOut ) {
       delay(1);
       
       --countTimeOut;
@@ -55,8 +55,7 @@ void loop() {
 
         for( char n = 0; n < PINOUT_LENGTH; n++) {
 
-          analogWrite(arduinoPins[n].motPin, LOW);
-
+          digitalWrite(arduinoPins[n].motPin, 0);
         }
       }
     }
@@ -71,23 +70,28 @@ void loop() {
   
   //Obtenção e manipulação com os valores da Serial
   for ( int nCount = 0; nCount < serialAvailable; nCount++ ) {
-    Serial.println(nCount);
+    
     char newValue = Serial.read();
 
     //Verificando os comandos caso tenho sido encontrado 'Z' previamente
-    if (m_nFrameDataPos >= 0) { 
+    if ( m_nFrameDataPos >= 0 ) { 
       
       //Unicos valores relevantes ao Arduino estao entre 8ª e 13ª posiçao inclusive
       if ( (m_nFrameDataPos >= (PINOUT_LENGTH + 1) ) && (m_nFrameDataPos < (2 * PINOUT_LENGTH + 1) ) ) {
-        
+
         //Verifica se está dentro dos niveis aceitos de OUTPUT - 'A' até 'P'
         if ( (newValue >= 'A') && (newValue < 'A' + OUTPUT_LEVELS)) {
           
           //Determinar nivel numerico do OUTPUT
           arduinoPins[m_nFrameDataPos - 7].motOutputLevel = 255 - (newValue - 'A') * (OUTPUT_LEVELS + 1);
-          
         }
-        else {
+        else if( newValue == 'Z' ) {
+          
+          Serial.println("E1");
+          
+          m_nFrameDataPos = 0;
+        }
+        else {          
           
           Serial.println("E2");
           
@@ -104,50 +108,31 @@ void loop() {
         //Fim de um frame de dados da Serial
         m_nFrameDataPos = -1; 
         
-        countTimeOut = 200;
+        countTimeOut = 100;
+        
       }
     }
     //Procurando inicio dos comandos pros motores
     else { 
-      Serial.println("Novo");
-      if (newValue == 'Z') {
+     
+      if ( newValue == 'Z' ) {
         
         //sync ok - Prepara para poder verificar os comandos
         m_nFrameDataPos = 0; 
       }
       else {
         
-        Serial.println("E1");
+        Serial.print("E3");
       }
     }
     
-    if ( countTimeOut ) { break; }
-  }
-  
-  
-  
-  
-  if (countTimeOut > 0) {
- 
-    countTimeOut--;
-  
-    for( char mPin = 0; mPin < PINOUT_LENGTH; mPin++) {
+    if ( countTimeOut ) { 
       
-      analogWrite(arduinoPins[mPin].motPin, arduinoPins[mPin].motOutputLevel);
+      for( char mPin = 0; mPin < PINOUT_LENGTH; mPin++) {
 
+        analogWrite(arduinoPins[mPin].motPin, arduinoPins[mPin].motOutputLevel);
+      }
+      break; 
     }
-
   }
-  else { //Caso tenha passado determinado tempo sem receber instruções pela Serial
-    
-    for( char mPin = 0; mPin < PINOUT_LENGTH; mPin++) {
-
-      analogWrite(arduinoPins[mPin].motPin, LOW);
-
-    }
-
-  }
-
-
-
 }
